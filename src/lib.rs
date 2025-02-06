480d53c36c7edd78a6892007fd102845d6db2420
jfairfie <jfairfield00@gmail.com>
2025-02-05 21:09:05 -0600

    Add search and save functionality with multi-key handling
    Introduce multi-key handling to enable search and save features. The `LWin+S` combination saves the current file, while `LWin+F` toggles the search bar. Refactor and expand file state management to support open files and ensure robust state handling.

--- src/lib.rs
+++ src/lib.rs
@@ -1,91 +1,25 @@
-use winit::dpi::{LogicalSize, PhysicalSize};
-use winit::event_loop::EventLoop;
-use winit::window::{Window, WindowBuilder};
-
-const WIDTH: u32 = 320;
-const HEIGHT: u32 = 240;
-const BOX_SIZE: i16 = 64;
-
-#[derive(Debug)]
-pub struct BoxState {
-    box_x: i16,
-    box_y: i16,
-    velocity_x: i16,
-    velocity_y: i16,
+#[derive(Debug, Clone)]
+pub struct FileState {
+    pub open_files: Vec<OpenFile>,
 }
 
-
-#[derive(Debug)]
-pub struct State {
-    pub window: Window,
-    pub user_state: UserState,
-    pub box_state: BoxState,
+#[derive(Debug, Clone)]
+pub struct OpenFile {
+    pub file_path: String,
+    pub current_file: bool
 }
 
-#[derive(Debug)]
-pub struct UserState {
-    pub pos_x: i32,
-    pub pos_y: i32,
-}
-
-impl State {
-    pub async fn new(event_loop: &EventLoop<()>) -> State {
-        let window = WindowBuilder::new()
-            .with_title("Rendering a Box with Pixels")
-            .with_inner_size(LogicalSize::new(WIDTH, HEIGHT))
-            .build(event_loop)
-            .unwrap();
-
+impl FileState {
+    pub fn new() -> Self {
         Self {
-            window,
-            user_state: UserState { pos_x: 0, pos_y: 0 },
-            box_state: BoxState {
-                box_x: 24,
-                box_y: 16,
-                velocity_x: 1,
-                velocity_y: 1,
-            },
+            open_files: vec![],
         }
     }
 
-    pub fn redraw(&self) {
-        self.window.request_redraw();
-    }
-
-    pub fn inner_size(&self) -> PhysicalSize<u32> {
-        self.inner_size()
-    }
-}
-
-impl BoxState {
-    pub fn move_box(&mut self) {
-        self.box_x += 1;
-        self.box_y += 1;
-    }
-}
-
-impl UserState {
-    pub fn default() -> Self {
-        Self {
-            pos_x: 0,
-            pos_y: 0,
-        }
-    }
-
-    pub fn new(x: i32, y: i32) -> Self {
-        Self {
-            pos_x: x,
-            pos_y: y
-        }
-    }
-
-    pub fn move_user(&mut self, x: Option<i32>, y: Option<i32>) {
-        if x.is_some() {
-            self.pos_x += x.unwrap();
-        }
-
-        if y.is_some() {
-            self.pos_y += y.unwrap();
-        }
+    pub fn insert_file(&mut self, path: String) {
+        self.open_files.push(OpenFile {
+            file_path: path,
+            current_file: true
+        });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+mod actions;
+
 use std::{fs, iter};
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::Write;
+use std::sync::Mutex;
 use egui::{FontDefinitions, Ui};
 use egui_wgpu_backend::ScreenDescriptor;
 use egui_winit_platform::{Platform, PlatformDescriptor};
 use rfd::FileDialog;
 use winit::event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
+use IDK::FileState;
 
 fn main() {
     let event_loop = EventLoop::new();
@@ -62,9 +69,15 @@ fn main() {
 
     let mut egui_rpass = egui_wgpu_backend::RenderPass::new(&device, surface_format, 1);
 
-    let mut name = String::new();
+    let mut search = String::new();
     let mut text_body = String::new();
     let mut open_modal = false;
+    let mut search_open = false;
+    let mut pressed_timer: u8 = 0;
+
+    let mut file_state = Mutex::new(FileState::new());
+
+    let mut pressed_keys: HashSet<VirtualKeyCode> = HashSet::new();
 
     let _ = event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
@@ -91,14 +104,28 @@ fn main() {
 
                 let context = platform.context();
 
-                egui::CentralPanel::default().show(&context, |ui| {
-                    ui.label("Enter your name");
-                    ui.text_edit_singleline(&mut name);
+                if pressed_keys.len() > 1 && pressed_timer == 4 {
+                    process_multiple_keys(&mut pressed_keys, &mut file_state, &mut text_body, &mut search_open);
+                    pressed_timer = 0;
+                } else if pressed_keys.len() > 1 {
+                    pressed_timer += 1;
+                } else if pressed_keys {
+                    pressed_timer = 0;
+                }
 
-                    if ui.button("Greet me!").clicked() {
-                        println!("Clicked");
+                egui::CentralPanel::default().show(&context, |ui| {
+                    if search_open {
+                        ui.label("Search");
+                        ui.text_edit_singleline(&mut search);
+
+                        if search != "" && text_body != "" {
+                            if text_body.contains(search.as_str()) {
+
+                            }
+                        }
                     }
 
+                    ui.add_space(20.0);
                     ui.text_edit_multiline(&mut text_body);
                 });
 
@@ -107,9 +134,8 @@ fn main() {
                         .collapsible(false)
                         .resizable(false)
                         .show(&context, |ui| {
-                            open_file_modal(ui, &mut open_modal, &mut text_body);
+                            open_file_modal(ui, &mut open_modal, &mut file_state, &mut text_body);
                         });
-
                 }
 
                 egui::TopBottomPanel::top("menu_bar").show(&context, |ui| {
@@ -180,22 +206,50 @@ fn main() {
                     WindowEvent::KeyboardInput { device_id, input, is_synthetic} => {
                         match input {
                             KeyboardInput {
+                                state: ElementState::Released,
+                                virtual_keycode: Some(key),
+                                ..
+                            } => {
+                                match key {
+                                    VirtualKeyCode::S => {
+                                        pressed_keys.remove(&VirtualKeyCode::S);
+                                    }
+                                    VirtualKeyCode::F => {
+                                        pressed_keys.remove(&VirtualKeyCode::F);
+                                    }
+                                    VirtualKeyCode::LShift => {
+                                        pressed_keys.remove(&VirtualKeyCode::LShift);
+                                    }
+                                    VirtualKeyCode::LWin => {
+                                        pressed_keys.remove(&VirtualKeyCode::LWin);
+                                    }
+                                    _ => {}
+                                }
+                            }
+
+                            KeyboardInput {
                                 state: ElementState::Pressed,
                                 virtual_keycode: Some(key),
                                 ..
                             } => {
                                 match key {
                                     VirtualKeyCode::A => {
-
+                                    }
+                                    VirtualKeyCode::F => {
+                                        pressed_keys.insert(VirtualKeyCode::F);
                                     }
                                     VirtualKeyCode::S => {
-
+                                        pressed_keys.insert(VirtualKeyCode::S);
+                                    }
+                                    VirtualKeyCode::LShift => {
+                                        pressed_keys.insert(VirtualKeyCode::LShift);
                                     }
                                     VirtualKeyCode::D => {
-
                                     }
                                     VirtualKeyCode::W => {
-
+                                    }
+                                    VirtualKeyCode::LWin => {
+                                        pressed_keys.insert(VirtualKeyCode::LWin);
                                     }
                                     VirtualKeyCode::Escape => {
                                         *control_flow = ControlFlow::Exit;
@@ -218,13 +272,55 @@ fn main() {
     });
 }
 
-fn open_file_modal(ui: &mut Ui, show_modal: &mut bool, text_body: &mut String) {
+fn process_multiple_keys(pressed_keys: &mut HashSet<VirtualKeyCode>, file_state: &mut Mutex<FileState>, text_body: &mut String, search_open: &mut bool) {
+    dbg!("Processing");
+    if pressed_keys.contains(&VirtualKeyCode::LWin) && pressed_keys.contains(&VirtualKeyCode::S) {
+        save_current_file(pressed_keys, file_state, text_body);
+    } else if pressed_keys.contains(&VirtualKeyCode::LWin) && pressed_keys.contains(&VirtualKeyCode::LShift) && pressed_keys.contains(&VirtualKeyCode::F) {
+        dbg!("Searching through text");
+    } else if pressed_keys.contains(&VirtualKeyCode::LWin) && pressed_keys.contains(&VirtualKeyCode::F)  {
+        *search_open = if *search_open { false } else { true };
+        // dbg!("Searching within file");
+    }
+
+    pressed_keys.clear();
+}
+
+fn save_current_file(pressed_keys: &mut HashSet<VirtualKeyCode>, file_state: &mut Mutex<FileState>, text_body: &mut String) {
+    dbg!("Saving");
+    {
+        let state = file_state.lock().unwrap();
+
+        for file in state.open_files.clone() {
+            if file.current_file {
+                let file = File::create(file.file_path);
+
+                match file {
+                    Ok(mut file) => {
+                        match file.write_all(text_body.as_bytes()) {
+                            Ok(_) => { }
+                            Err(err) => { eprintln!("Error saving file {}", err)}
+                        }
+                    }
+                    Err(err) => { eprintln!("Error opening file {}", err); }
+                }
+            }
+        }
+    }
+}
+
+fn open_file_modal(ui: &mut Ui, show_modal: &mut bool, file_state: &mut Mutex<FileState>, text_body: &mut String) {
     ui.label("Select file to open...");
 
     if ui.button("Browser...").clicked() {
         if let Some(path) = FileDialog::new().pick_file() {
             match fs::read_to_string(&path) {
                 Ok(content) => {
+                    {
+                        let mut file_state = file_state.lock().unwrap();
+                        file_state.insert_file(path.to_str().unwrap().to_string());
+                    }
+
                     *text_body = content;
                 }
                 Err(err) => {
